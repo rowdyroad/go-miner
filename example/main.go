cea4a035079cf4b8ed86169b496efa20ca3db55e
b.strelchik <b.strelchik@gmail.com>
2019-09-29 00:42:32 +0300

    + example

---
+++ example/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"crypto/sha256"
+	"log"
+	"time"
+
+	"github.com/rowdyroad/go-miner"
+)
+
+func main() {
+	c := miner.NewMiner(1024, time.Second, func(s []byte) []byte {
+		hash := sha256.Sum256(s)
+		return hash[:]
+	})
+
+	for {
+		tt := time.Now()
+		hash, nonce := c.GetHashAndNonce(10*time.Second, []byte("hello"))
+		checkHash := sha256.Sum256(append([]byte("hello"), nonce...))
+		log.Println("OK", time.Now().Sub(tt), hash, checkHash)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rowdyroad/go-miner
+
+require github.com/stretchr/testify v1.4.0
